fdcc136680b786ad81d38bf64f6a59cd43a6fec8
Daniel Alves <alvesdesouza.daniel@gmail.com>
2017-01-18 23:56:57 -0200

    Implementacao dos produtos da Home no .Service

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,10 @@
 angular.module('app.controllers', [])
 
-.controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('homeCtrl', ['$scope', '$stateParams', 'productStore', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
+function ($scope, $stateParams, productStore) {
+$scope.products = productStore.getProduct();
 
 
 }])
--- www/js/services.js
+++ www/js/services.js
@@ -7,14 +7,58 @@ angular.module('app.services', [])
 
 }])
 
+.service('productStore', [function(){
+
+    var allProducts = [
+        {
+            productId:0,
+            productName:"Pen Drive 64GB - Kingston",
+            productImage:"http://images.tcdn.com.br/img/img_prod/325348/pen_drive_64gb_kingston_dt101g2_64gb_3844_3_20150223115418.jpg",
+            productPrice:58.90,
+            productQut:55
+        },
+        {
+            productId:1,
+            productName:"Taco de Golf (ferro) - Excel",
+            productImage:"https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcT2A5GRrZtq3IWa3w_LzgB5_qvl040QFOkc_4j2jC7HpyygiaE&usqp=CAE",
+            productPrice:337.40,
+            productQut:230
+        },
+        {
+            productId:2,
+            productName:"Cookies Original - Bauduco",
+            productImage:"http://www.cosmedamiaocomercio.com.br/site/uploads/produto/34f846082992704cedffa5561f041c71.png",
+            productPrice:5.80,
+            productQut:15
+        },
+        {
+            productId:3,
+            productName:"Arroz Integral Orgânico - Tio João",
+            productImage:"http://www.paodeacucar.com/img/uploads/1/758/510758.jpg",
+            productPrice:22.90,
+            productQut:84
+        }
+        ];
+
+        var productsFuncs = {
+
+            getProduct:function(){
+                console.log("Get Product");
+                console.log(allProducts);
+                return allProducts;
+            }
+        }
+        return productsFuncs;
+}])
+
 
 .service('signUp', [function(){
   var allDataSignUp = [];
   var signUpFuncs = {
 
       getPerson:function(){
-          console.log("Get Product");
-          console.log(allDataSignUp);
+          // console.log("Get Product");
+          // console.log(allDataSignUp);
           return allDataSignUp;
       },
 
