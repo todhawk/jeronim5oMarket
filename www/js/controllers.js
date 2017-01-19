d29eace2a373ea23389fc6908385b7f646c93b74
Rapha Prieto <fernandesprieto@gmail.com>
2017-01-19 17:28:00 -0200

    servico p/ os produtos atualizados

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,11 @@
 angular.module('app.controllers', [])
 
-.controller('homeCtrl', ['$scope', '$stateParams', 'productStore', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('homeCtrl', ['$scope', '$stateParams', 'productService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, productStore) {
+function ($scope, $stateParams, productService) {
 $scope.products = productStore.getProduct();
+
 
 
 }])
