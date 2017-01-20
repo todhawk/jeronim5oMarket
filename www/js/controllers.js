56e313c4e7966181976b1ee7f522eb868829b539
Jose Kim <josekim@Joses-MacBook-Air.local>
2017-01-20 10:40:19 -0200

    botoes adicionados

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,10 @@
 angular.module('app.controllers', [])
 
-
-.controller('homeCtrl', ['$scope', '$stateParams', 'productService', '$state',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, productService, $state) {
-    $scope.products = productService.getProduct();
+function ($scope, $stateParams) {
 
-    $scope.addCart = function(item){
-        productService.carregaCarrinho(item);
-    }
-
-    $scope.descricao = function (item) {
-        productService.selectProduct(item);
-        $state.go('menu.item');
-    }
 
 
 }])
@@ -27,16 +17,13 @@ function ($scope, $stateParams) {
 
 }])
 
-.controller('loginCtrl', ['$scope', '$stateParams', '$state','userInfo', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('loginCtrl', ['$scope', '$stateParams', '$state', 'login', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, $state, userInfo) {
-    $scope.infoSignUp = userInfo.getUser();
-    // console.log
+function ($scope, $stateParams, $state, login) {
     $scope.login = function(user,password){
         //console.log("User: " + user + " Senha: " + password);
-        // if(user == userInfo.infoSignUp.userName && password == userInfo.infoSignUp.userMail){
-        if (user == 'jeronimo'&& password == 'lindao') {
+        if(user == "admin" && password == "admin"){
             //Redirecionar para a pagina Home
             //console.log("Login realizado com sucesso!!!");
             $scope.error = false;
@@ -49,34 +36,31 @@ function ($scope, $stateParams, $state, userInfo) {
 
 
     }
+
+
+
+
+
+
+
 }])
 
-.controller('signupCtrl', ['$scope', '$stateParams', '$state', 'userInfo', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('signupCtrl', ['$scope', '$stateParams', '$state', 'login', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, $state, userInfo) {
-    $scope.signup = function(name,email,password){//função vinculada ao btn signup
-        // console.log(name + " - " + email + " - " + password);
-        userInfo.addUser(name,email,password);
+function ($scope, $stateParams, $state, login) {
+    $scope.signup = function(name,email,password){
+        login.addUser(name,email,password);
         $state.go("menu.home");
-    };
-
+    }
 
 }])
 
-.controller('itemCtrl', ['$scope', '$stateParams', '$state', 'productService',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('itemCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, $state, productService) {
-  $scope.product = productService.getSelectedProduct();
+function ($scope, $stateParams) {
 
-  $scope.addCart = function(item){
-      productService.carregaCarrinho(item);
-  }
-
-  $scope.vaiPraHome = function() {
-    $state.go("menu.home");
-  }
 
 }])
 
@@ -84,6 +68,7 @@ function ($scope, $stateParams, $state, productService) {
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
+  
 
 
 }])
